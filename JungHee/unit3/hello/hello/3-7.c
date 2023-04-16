72181c07a7311a86fa01798f4c980cca16c28576
junghee.lee <junghee.lee@playlinks.com>
2023-04-16 20:37:51 +0900

    [junghe] 4강까지 완료

---
+++ JungHee/unit3/hello/hello/3-7.c
@@ -0,0 +1,13 @@
+/*
+	Hello, world! , 1234567890이 각 줄에 출력되게 만드세요
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, world\n");
+	printf("1234567890\n");
+
+	return 0;
+}
---
+++ JungHee/unit3/hello/hello/3-8.c
@@ -0,0 +1,13 @@
+/*
+	다음 소스 코드를 완성하여 "Hello, world!"가 출력되게 만드세요.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	//printf("Hello, %s\n", _______);
+	printf("Hello, %s\n", "world!");
+
+	return 0;
+}
